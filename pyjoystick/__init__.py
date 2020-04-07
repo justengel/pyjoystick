fbd39edfa60be1807ebfda868484b9c44c1d23c2
justengel <jtengel08@gmail.com>
2020-04-07 10:32:07 -0400

    Added Stash to the main pyjoystick __init__.py

--- pyjoystick/__init__.py
+++ pyjoystick/__init__.py
@@ -1,6 +1,7 @@
 from .__meta__ import version as __version__
 
 from .utils import PYJOYSTICK_DIR, deadband, change_path, rescale, PeriodicThread
+from .stash import Stash
 from .button_repeater import Repeater, ButtonRepeater, HatRepeater, ButtonHatRepeater
 from .interface import Key, Joystick
 
